4743d273dbe978add20c6eaa965445f77b46cbc2
Omniboy <kachorritou@gmail.com>
2019-07-22 18:16:05 -0300

    added inventory

---
+++ scripts/heroes.js
@@ -0,0 +1,111 @@
+// Creamos la constante que contiene los personajes. Es un
+// array que contiene n objetos. Cada uno, es un personaje
+// que el jugador puede elegir para jugar.
+const personajes = [
+    new Personaje(
+      "Guerrero",
+      estadisticas = {
+        ataque: 60,
+        defensa: 50,
+        maxVida: 600,
+        vida: 600,
+        maxMana: 1000,
+        mana: 1000,
+        portrait: "img/heroes/guerrero.png"
+      },
+      [1, 2, 0, 4]
+    ),
+    new Personaje(
+      "Mago",
+      estadisticas = {
+        ataque: 30,
+        defensa: 20,
+        maxVida: 400,
+        vida: 400,
+        maxMana: 2500,
+        mana: 2500,
+        portrait: "img/heroes/mago.png"
+      },
+      [1, 3, 0, 4]
+    ),
+    new Personaje(
+      "Asesino",
+      estadisticas = {
+        ataque: 70,
+        defensa: 30,
+        maxVida: 500,
+        vida: 500,
+        maxMana: 500,
+        mana: 500,
+        portrait: "img/heroes/Actor1-3.png"
+      },
+      [1, 2, 0, 4]
+    ),
+    new Personaje(
+      "Sacerdota",
+      estadisticas = {
+        ataque: 40,
+        defensa: 30,
+        maxVida: 500,
+        vida: 500,
+        maxMana: 1500,
+        mana: 1500,
+        portrait: "img/heroes/Actor3-4.png"
+      },
+      [1, 2, 0, 4]
+    ),
+    new Personaje(
+      "Guerrera",
+      estadisticas = {
+        ataque: 60,
+        defensa: 50,
+        maxVida: 600,
+        vida: 600,
+        maxMana: 1000,
+        mana: 1000,
+        portrait: "img/heroes/Actor1-2.png"
+      },
+      [1, 2, 0, 4]
+    ),
+    new Personaje(
+      "Asesino",
+      estadisticas = {
+        ataque: 70,
+        defensa: 30,
+        maxVida: 500,
+        vida: 500,
+        maxMana: 500,
+        mana: 500,
+        portrait: "img/heroes/Actor1-3.png"
+      },
+      [1, 2, 0, 4]
+    ),
+    new Personaje(
+      "Sacerdota",
+      estadisticas = {
+        ataque: 40,
+        defensa: 30,
+        maxVida: 500,
+        vida: 500,
+        maxMana: 1500,
+        mana: 1500,
+        portrait: "img/heroes/Actor3-4.png"
+      },
+      [1, 2, 0, 4]
+    )
+  ]
+
+// Creamos el objeto personalizado 'Personaje'.
+// Este va a tener las propiedades que corresponden,
+// y los metodos, como 'atacar', etc.
+function Personaje(tipo, estadisticas, habilidades, enemigo = false) {
+    this.tipo = tipo
+    this.estadisticas = estadisticas
+    this.esEnemigo = enemigo
+    this.habilidades = [
+      libro[habilidades[0]],
+      libro[habilidades[1]],
+      libro[habilidades[2]],
+      libro[habilidades[3]]
+    ]
+  }
---
+++ scripts/monster.js
@@ -0,0 +1,58 @@
+const enemigos = [
+    new Personaje(
+      "Orco",
+      estadisticas = {
+        ataque: 50,
+        defensa: 30,
+        maxVida: 250,
+        vida: 250,
+        maxMana: 750,
+        mana: 750,
+        portrait: "img/enemigos/orco.png"
+      },
+      [0, 1, 1, 1],
+      true
+    ),
+    new Personaje(
+      "Slime",
+      estadisticas = {
+        ataque: 70,
+        defensa: 20,
+        maxVida: 200,
+        vida: 200,
+        maxMana: 500,
+        mana: 500,
+        portrait: "img/enemigos/slime.png"
+      },
+      [3, 0, 0, 1],
+      true
+    ),
+    new Personaje(
+      "Demonio",
+      estadisticas = {
+        ataque: 150,
+        defensa: 80,
+        maxVida: 400,
+        vida: 400,
+        maxMana: 2500,
+        mana: 2500,
+        portrait: "img/enemigos/demonio.png"
+      },
+      [3, 3, 1, 0],
+      true
+    ),
+    new Personaje(
+      "Esqueleto",
+      estadisticas = {
+        ataque: 100,
+        defensa: 20,
+        maxVida: 600,
+        vida: 600,
+        maxMana: 250,
+        mana: 250,
+        portrait: "img/enemigos/esqueleto.png"
+      },
+      [1, 1, 1, 2],
+      true
+    )
+  ]
